c563897c536718d6c7da8b7fe3f767dca39faaff
Batuhan Derya <bderya@3-F-14.42heilbronn.de>
2023-01-03 10:50:03 +0100

    Last

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: bderya <bderya@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 08:21:08 by bderya            #+#    #+#             */
-/*   Updated: 2023/01/03 09:21:58 by bderya           ###   ########.fr       */
+/*   Updated: 2023/01/03 10:16:47 by bderya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,26 @@
 
 int main(int argc, char **argv)
 {
+	int lengt;
 	if(argc == 1)
 		std::cout<<"* LOUD AND UNBERABLE FEEDBACK NOISE *"<<std::endl;
 	else
 	{
 		for(int i = 1; i < argc; i++)
 		{
-			int lengt = sizeof(argv[i]);
-			char write[lengt];
-	
+			lengt = strlen(argv[i]);
+			
 			for(int j = 0; j < lengt; j++)
-				write[j] = toupper(argv[i][j]);
-				
-			std::cout<<write<<" ";
+			{
+				if (isalpha(argv[i][j]))
+					std::cout<<(char)toupper(argv[i][j]);
+				else
+					std::cout<<(char)argv[i][j];
+			}
+			std::cout<<" ";
 		}
+		std::cout<<std::endl;
 	}
+	
 	return (0);
 }
