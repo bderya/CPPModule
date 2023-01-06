1758ac7369792f4d1f34a0928ea8484b38e5ae45
Batuhan Derya <bderya@3-F-14.42heilbronn.de>
2023-01-06 13:59:15 +0100

    ex02 need mod

---
+++ CPP00/ex02/Account.cpp
@@ -0,0 +1,15 @@
+#include "Account.hpp"
+#include <iostream>
+
+Account::Account()
+{
+	t acc;
+
+	_accountIndex = 0;
+	_amount = 0;
+}
+
+Account::~Account(){}
+
+
+int Account::getNbAccounts(void){}
